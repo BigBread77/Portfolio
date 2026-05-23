a5074b8885eccacb9e2dc7ac8779512e09eea236
Patrick <bread@Patricks-Mac-mini.local>
2026-05-22 23:53:08 -0400

    Fixed photos

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,5 +1,9 @@
 import ProjectCard from "./ProjectCard"
 import './Projects.css'
+import travelImg from '../assets/952456172.webp'
+import memoryImg from '../assets/milad-fakurian-unsplash-1200x628.jpg'
+import ecommerceImg from '../assets/shopping_2.jpg'
+import analyticsImg from '../assets/herramientas-de-big-data.jpeg'
 
 
 function Projects(){
@@ -7,10 +11,10 @@ function Projects(){
     <div className="project-container">
       <h4 className="project-label">PROJECTS:</h4>
       <div className="project-column">
-        <ProjectCard name="Travel Agency" img="src/assets/952456172.webp" path= "/travelagency" />
-        <ProjectCard name="Memory Game" img="src/assets/milad-fakurian-unsplash-1200x628.jpg" path = "/memorygame" />
-        <ProjectCard name="Ecommerce" img="src/assets/shopping_2.jpg" path = "/ecommerce" />
-        <ProjectCard name="Analytics" img="src/assets/herramientas-de-big-data.jpeg" path= "/analytics"/>
+        <ProjectCard name="Travel Agency" img={travelImg} path= "/travelagency" />
+        <ProjectCard name="Memory Game" img={memoryImg} path = "/memorygame" />
+        <ProjectCard name="Ecommerce" img={ecommerceImg} path = "/ecommerce" />
+        <ProjectCard name="Analytics" img={analyticsImg} path= "/analytics"/>
 
 
       </div>
